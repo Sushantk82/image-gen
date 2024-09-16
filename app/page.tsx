44fdf0c2ec927368ef4a04d49b4974df9224d56b
Sushant Kumar <sushant@Sushants-MacBook-Air.local>
2024-09-16 12:59:04 +0530

    up

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-4xl font-bold mb-4">Hello everyone! This is Sushant Kumar</h1>
-        <p className="text-lg text-gray-600">Welcome to our image generation app!</p>
+        <p className="text-lg text-gray-600">Welcome to our image generation app!adfasdf</p>
       </main>
       
     </div>
